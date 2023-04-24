9d42c753a24d05adb958176e3ebabdabd60d88b8
eroyappa <110120200+eroyappa@users.noreply.github.com>
2023-04-24 12:54:43 +0000

    Test

--- LilL_Monsters_Site/js/script.js
+++ LilL_Monsters_Site/js/script.js
@@ -105,7 +105,7 @@ $(function () {
   new ScrollMagic.Scene({
     triggerElement: '#types',
     triggerHook: 0,
-    duration: 600,
+    duration: 300,
   })
     .setPin('#types')
     .setTween(typesTween)
